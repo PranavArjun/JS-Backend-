f9e228fa1f8619f4881a14274dfc218695fdfdb0
Arjun <2322492@cognizant.com>
2024-06-19 14:21:51 +0530

    Project Setup and Database Connection

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+import { Promise } from "mongoose"
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
